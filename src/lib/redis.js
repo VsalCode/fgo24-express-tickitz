15bc9566419b5779bb9bb3feeebd7d838806531c
VsalCode <vsal.ilham@gmail.com>
2025-07-19 22:19:50 +0700

    chore: add redis client instalation and configuration

---
+++ src/lib/redis.js
@@ -0,0 +1,12 @@
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  db: process.env.REDIS_DB,
+});
+
+module.exports = {
+  redisClient
+};
