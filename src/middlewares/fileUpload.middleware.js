be2a0f9462535c6f7e87cdb22ac67c1d6c96c9f2
VsalCode <vsal.ilham@gmail.com>
2025-07-21 00:38:05 +0700

    fix(middleware): improve file uploads middleware for handle multiple fields

--- src/middlewares/fileUpload.middleware.js
+++ src/middlewares/fileUpload.middleware.js
@@ -2,31 +2,62 @@ const path = require("node:path");
 const { v4: uuidv4 } = require('uuid');
 const multer = require("multer");
 
-const posterStorage = multer.diskStorage({
-   destination: (req, file, cb) => {
-    cb(null, path.join("uploads", "poster-image"));
-   },
-   filename: (req, file, cb) => {
+const movieStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (file.fieldname === 'poster') {
+      cb(null, path.join("uploads", "poster-image"));
+    } else if (file.fieldname === 'backdrop') {
+      cb(null, path.join("uploads", "backdrop-image"));
+    } else {
+      cb(new Error('Unexpected field name'), false);
+    }
+  },
+  filename: (req, file, cb) => {
     const fileName = file.originalname;
-    const ext = fileName.split(".")[1];
-    const savedFile = `${uuidv4()}.${ext}`;
+    const ext = path.extname(fileName); 
+    const savedFile = `${uuidv4()}${ext}`;
     cb(null, savedFile);
   }
 });
 
-const backdropStorage = multer.diskStorage({
-   destination: (req, file, cb) => {
-    cb(null, path.join("uploads", "backdrop-image"));
-   },
-   filename: (req, file, cb) => {
-    const fileName = file.originalname;
-    const ext = fileName.split(".")[1];
-    const savedFile = `${uuidv4()}.${ext}`;
-    cb(null, savedFile);
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only image files are allowed!'), false);
   }
+};
+
+const fileUpload = multer({ 
+  storage: movieStorage,
+  limits: {
+    fileSize: 5 * 1024 * 1024 
+  },
+  fileFilter: fileFilter
 });
 
 module.exports = {
-  posterStorage,
-  backdropStorage
+  fileUpload,
+  posterStorage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join("uploads", "poster-image"));
+    },
+    filename: (req, file, cb) => {
+      const fileName = file.originalname;
+      const ext = path.extname(fileName);
+      const savedFile = `${uuidv4()}${ext}`;
+      cb(null, savedFile);
+    }
+  }),
+  backdropStorage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join("uploads", "backdrop-image"));
+    },
+    filename: (req, file, cb) => {
+      const fileName = file.originalname;
+      const ext = path.extname(fileName);
+      const savedFile = `${uuidv4()}${ext}`;
+      cb(null, savedFile);
+    }
+  })
 };
