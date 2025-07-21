b347cf28fae80a43d293638dd661cf516cb55a87
VsalCode <vsal.ilham@gmail.com>
2025-07-21 10:53:44 +0700

    feat(movies): add movie detail endpoint

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -5,16 +5,16 @@ exports.getAllMovies = async function (_, res) {
   try {
     const moviesData = await movies.findAll({
       include: [
-        { model: genres, as: 'genres' },
-        { model: casts, as: 'casts' },
-        { model: directors, as: 'directors' }
-      ]
+        { model: genres, as: "genres" },
+        { model: casts, as: "casts" },
+        { model: directors, as: "directors" },
+      ],
     });
 
     return res.status(http.HTTP_STATUS_OK).json({
       success: true,
       message: "Get all movies successfully!",
-      results: moviesData.map(movie => ({
+      results: moviesData.map((movie) => ({
         id: movie.id,
         title: movie.title,
         overview: movie.overview,
@@ -23,25 +23,83 @@ exports.getAllMovies = async function (_, res) {
         release_date: movie.release_date,
         runtime: movie.runtime,
         vote_average: movie.vote_average,
-        genres: movie.genres.map(genre => ({
+        genres: movie.genres.map((genre) => ({
           id: genre.id,
-          name: genre.name
+          name: genre.name,
         })),
-        casts: movie.casts.map(cast => ({
+        casts: movie.casts.map((cast) => ({
           id: cast.id,
-          name: cast.name
+          name: cast.name,
         })),
-        directors: movie.directors.map(director => ({
+        directors: movie.directors.map((director) => ({
           id: director.id,
-          name: director.name
-        }))
-      }))
+          name: director.name,
+        })),
+      })),
     });
-
   } catch (err) {
     return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
       success: false,
       message: "Failed to retrieve movies",
+      errors: err.message,
+    });
+  }
+};
+
+exports.getMovieDetail = async function (req, res) {
+  try {
+    const movieIdParams = req.params.id;
+    const movieId = parseInt(movieIdParams);
+
+    const moviesData = await movies.findAll(
+      {
+        include: [
+          { model: genres, as: "genres" },
+          { model: casts, as: "casts" },
+          { model: directors, as: "directors" },
+        ],
+      },
+      { where: { id: movieId } }
+    );
+    if (!moviesData) {
+      return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: `movies with id ${movieId} not found!`,
+      });
+    }
+
+    const movie = moviesData[0];
+
+    return res.status(http.HTTP_STATUS_OK).json({
+      success: true,
+      message: `Get movies with id ${movieId} successfully!`,
+      results: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        poster_path: movie.poster_path,
+        release_date: movie.release_date,
+        runtime: movie.runtime,
+        vote_average: movie.vote_average,
+        genres: movie.genres.map((genre) => ({
+          id: genre.id,
+          name: genre.name,
+        })),
+        casts: movie.casts.map((cast) => ({
+          id: cast.id,
+          name: cast.name,
+        })),
+        directors: movie.directors.map((director) => ({
+          id: director.id,
+          name: director.name,
+        })),
+      },
+    });
+  } catch (err) {
+    return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Failed to retrieve movie detail",
       errors: err.message,
     });
   }
--- src/routers/movies.router.js
+++ src/routers/movies.router.js
@@ -1,6 +1,7 @@
 const moviesRouter = require("express").Router(); 
 const moviesController = require("../controllers/movies.controller");
 
+moviesRouter.get("/:id", moviesController.getMovieDetail );
 moviesRouter.get("", moviesController.getAllMovies );
 moviesRouter.get("/genres", moviesController.getAllGenres );
 moviesRouter.get("/directors", moviesController.getAllDirectors );
